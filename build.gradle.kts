d2a12451df8c37db2dc36a43a7bd992c181f3168
Matthias Herrmann <matthias.herrmann@gmail.com>
2024-08-15 17:23:44 +0200

    Add configuration for swagger ui

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+	implementation("org.springdoc:springdoc-openapi-starter-common:2.6.0")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
---
+++ src/main/kotlin/de/os/dehaar/dokos_rest_api/config/ApiConfig.kt
@@ -0,0 +1,28 @@
+package de.os.dehaar.dokos_rest_api.config
+
+import io.swagger.v3.oas.models.ExternalDocumentation
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.info.License
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+class ApiConfig {
+
+    @Bean
+    fun springShopOpenAPI(): OpenAPI {
+        return OpenAPI()
+            .info(
+                Info()
+                    .title("DokOS REST API")
+                    .description("An API with the purpose of storing Doppelkopf results")
+                    .version("0.0.1-SNAPSHOT")
+                    .license(
+                        License().name("Apache 2.0").url("http://springdoc.org")
+                    )
+            )
+    }
+}
