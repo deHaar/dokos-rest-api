3dd0acc1f79054d23f3f8bb242849fe70f839869
Matthias Herrmann <matthias.herrmann@gmail.com>
2024-08-15 11:24:02 +0200

    Add initial project files

---
+++ src/main/kotlin/de/os/dehaar/dokos_rest_api/DokOsRestApiApplication.kt
@@ -0,0 +1,11 @@
+package de.os.dehaar.dokos_rest_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DokOsRestApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<DokOsRestApiApplication>(*args)
+}
---
+++ src/test/kotlin/de/os/dehaar/dokos_rest_api/DokOsRestApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.os.dehaar.dokos_rest_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DokOsRestApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
