a0e6ae45c1aeb2fb4d43c0d95bb7ac3f83d39a38
deHaar <matthias.herrmann@gmail.com>
2025-06-15 23:47:21 +0200

    Change attribute name

--- src/main/kotlin/de/os/dehaar/dokos/api/entity/Player.kt
+++ src/main/kotlin/de/os/dehaar/dokos/api/entity/Player.kt
@@ -8,8 +8,8 @@ import jakarta.validation.constraints.NotBlank
 class Player @JvmOverloads constructor(
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null,
+    @GeneratedValue(strategy = GenerationType.UUID)
+    val id: String? = null,
 
     @NotBlank
     val displayName: String,
