672b65dc17e9e3b40c8c98c0fdd6cb502bd473b0
deHaar <matthias.herrmann@gmail.com>
2025-06-15 23:04:54 +0200

    Add first container and compose related stuff

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dokos-rest-api
 A RESTful API for the purpose of storing results from games and rounds of the card game Doppelkopf, which is mainly played in northern Germany.
+
+## Build info
+1. build the Spring Boot project by executing the gradle wrapper in the root folder of the project as follows:
+```shell
+./gradlew clean build -x test
+```
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
 	kotlin("jvm") version "2.0.10"
 	kotlin("plugin.spring") version "2.0.10"
@@ -28,7 +30,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 	implementation("org.springdoc:springdoc-openapi-starter-common:2.6.0")
-	runtimeOnly("com.h2database:h2")
+	// runtimeOnly("com.h2database:h2")
+	implementation("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -37,6 +40,7 @@ dependencies {
 kotlin {
 	compilerOptions {
 		freeCompilerArgs.addAll("-Xjsr305=strict")
+		jvmTarget.set(JvmTarget.JVM_22)
 	}
 }
 
