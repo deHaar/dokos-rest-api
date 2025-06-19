3e47c3dec83119beafc74cac9bbea12e1f422619
deHaar <matthias.herrmann@gmail.com>
2025-06-19 22:34:32 +0200

    Remove misconfig (no test folders in the source set and the source set was default without the test folders)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,6 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.6"
 }
 
-sourceSets.main {
-	java.srcDirs("src/main/kotlin", "src/test/kotlin")
-}
-
 group = "de.os.dehaar"
 version = "0.0.1-SNAPSHOT"
 
@@ -35,16 +31,13 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 	implementation("org.springdoc:springdoc-openapi-starter-common:2.6.0")
-	// runtimeOnly("com.h2database:h2")
 	implementation("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
 	compilerOptions {
-		// jvmTarget()
 		jvmToolchain(22)
 		freeCompilerArgs.addAll("-Xjsr305=strict")
 	}
